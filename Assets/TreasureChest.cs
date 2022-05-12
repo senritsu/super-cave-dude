cdc2c82ec3d338f97171f0d3305b3bab596dd0b5
senritsu <teh.b4k4@gmail.com>
2022-05-12 20:51:18 +0200

    integrate first round of playtesting feedback

--- Assets/TreasureChest.cs
+++ Assets/TreasureChest.cs
@@ -5,10 +5,12 @@ public class TreasureChest : MonoBehaviour
     private Animator _animator;
     private static readonly int Open = Animator.StringToHash("Open");
     private ParticleSystem _particleSystem;
+    private BoxCollider2D _collider;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _collider = GetComponent<BoxCollider2D>();
         _particleSystem = GetComponent<ParticleSystem>();
     }
 
@@ -20,5 +22,6 @@ public class TreasureChest : MonoBehaviour
 
         _animator.SetTrigger(Open);
         _particleSystem.Play();
+        _collider.enabled = false;
     }
 }
